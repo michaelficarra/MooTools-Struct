9130179ef5dd84625f31248abfe8c17f53413eeb
Michael <michael@Hotaru.(none)>
2010-03-09 23:09:34 -0500

    Incorporating most changes suggested by Arian.

--- Source/Struct.js
+++ Source/Struct.js
@@ -6,11 +6,12 @@ license: LGPL
 
 authors:
 - Michael Ficarra
+- Arian Stolwijk
 
 requires:
 - core/1.2.4:Core
 - core/1.2.4:Array
-- core/1.2.4:Function
+- core/1.2.4:Hash
 - core/1.2.4:Class
 - core/1.2.4:Class.Extras
 
@@ -19,29 +20,45 @@ provides: [Struct]
 ...
 */
 var Struct = new Class({
+
 	Implements: Options,
 	options: {
 		getterPrefix: 'get',
 		setterPrefix: 'set'
 	},
+
 	initialize: function(){
 		var args = arguments, options;
-		if(arguments.length==2 && ['object','hash'].contains($type(arguments[1]))){
+		if(args.length==2 && ['object','hash'].contains($type(arguments[1]))){
 			args = arguments[0];
 			options = arguments[1];
 		}
-		args = $A($splat(args));
-		this.setOptions(options||{});
-		this.struct = new Class({
+		this.args = $splat(args);
+		this.setOptions(options);
+		this.struct = this.createStruct();
+		this.addAccessors(this.struct);
+		return this.struct;
+	},
+		
+	createStruct: function(){
+		var that = this;
+		return new Class({
 			_storage: {},
 			initialize: function(){
-				$A(arguments).each(function(arg,i){
-					this._storage[args[i]] = arg;
+				var args = arguments, len = args.length;
+				$each(that.args,function(arg,i){
+					this._storage[arg] = (i>=len ? undefined : args[i]);
 				}.bind(this));
 			},
-			members: function(){ return args; },
-			each: function(fn,bind){ return this.toHash().each(fn,bind); },
-			toHash: function(){ return $H(this._storage); },
+			members: function(){
+				return that.args;
+			},
+			each: function(fn,bind){
+				return $each(this._storage,fn,bind);
+			},
+			toHash: function(){
+				return $H(this._storage);
+			},
 			equals: function(other){
 				other = other.toHash();
 				return this.toHash().every(function(val,key){
@@ -49,23 +66,28 @@ var Struct = new Class({
 				});
 			}
 		});
-		args.each(function(arg){
-			var getFunc = function(){ return this._storage[arg]; },
-			setFunc = function(val){ this._storage[arg] = val; return this; },
-			getName, setName, baseName;
+	},
+
+	addAccessors: function(struct){
+		var prefixes = {get: this.options.getterPrefix, set: this.options.setterPrefix};
+		$each(this.args,function(arg){
+			var getName, setName, baseName, implement = {};
 			getName = setName = arg.toString().replace(/^[A-Z]/,function(m){return m.toLowerCase();}).replace('_','-').camelCase();
 			baseName = arg.toString().capitalize().replace('_','-').camelCase();
-			if(this.options.getterPrefix != '') getName = this.options.getterPrefix + baseName;
-			if(this.options.setterPrefix != '') setName = this.options.setterPrefix + baseName;
-			this.struct.implement($H().set(setName,setFunc).set(getName,getFunc).getClean());
+			if(prefixes.get != '') getName = prefixes.get + baseName;
+			if(prefixes.set != '') setName = prefixes.set + baseName;
+			implement[getName] = function(){ return this._storage[arg]; },
+			implement[setName] = function(val){ this._storage[arg] = val; return this; },
+			struct.implement(implement);
 		}.bind(this));
-		return this.struct;
 	}
 });
 
-Array.implement({toStruct:function(options){
-	return new Struct(this,options||{});
-}});
+Array.implement({
+	toStruct:function(options){
+		return new Struct(this,options);
+	}
+});
 
 /* Copyright 2010 Michael Ficarra
 This program is distributed under the (very open)
